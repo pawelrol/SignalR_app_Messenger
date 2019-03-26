4e734f43c3c445fc9d63a2f167396c2d983e9fb8
pawelrol <pavlo10@vp.pl>
2019-03-26 18:46:32 +0100

    first commit

---
+++ SignalApp/Data/Migrations/20190326171126_friends.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace SignalApp.Data.Migrations
+{
+    public partial class friends : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "FriendLists",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    UserId = table.Column<string>(nullable: true),
+                    FriendId = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_FriendLists", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "FriendLists");
+        }
+    }
+}
---
+++ SignalApp/Models/FriendList.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SignalApp.Models
+{
+    public class FriendList
+    {
+        public int Id { get; set; }
+        public string UserId { get; set; }
+        public string FriendId { get; set; }
+    }
+}
